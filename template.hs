3a8549e627216bbad6ddbd10a601af52124e8029
James Sully <sullyj3@gmail.com>
2019-12-11 00:06:05 +1100

    Add template

---
+++ template.hs
@@ -0,0 +1,19 @@
+import Flow
+
+main = do
+  input <- readFile "input.txt"
+  -- parse input here
+
+  putStrLn "part 1"
+  print $ part1 undefined
+
+  putStrLn ""
+
+  putStrLn "part 2"
+  print $ part2 undefined
+
+part1 :: _
+part1 = undefined
+
+part2 :: _
+part2 = undefined
