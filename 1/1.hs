0931afb139b2cc864c054049095e1a6b48f4286a
James Sully <sullyj3@gmail.com>
2019-12-10 19:27:44 +1100

    Day 1 part 2

--- 1/1.hs
+++ 1/1.hs
@@ -1,3 +1,4 @@
+import Data.List (unfoldr)
 
 main = do
   input <- readFile "input.txt"
@@ -5,10 +6,29 @@ main = do
   let masses :: [Int]
       masses = map read . lines $ input 
 
-  print $ day1 masses
+  putStrLn "part 1"
+  print $ part1 masses
+
+  putStrLn ""
+
+  putStrLn "part 2"
+  print $ part2 masses
 
 fuel :: Int -> Int
 fuel mass = mass `div` 3 - 2
 
-day1 :: [Int] -> Int
-day1 = sum . map fuel
+part1 :: [Int] -> Int
+part1 = sum . map fuel
+
+part2 :: [Int] -> Int
+part2 = sum . map part2Fuel
+
+part2Fuel :: Int -> Int
+part2Fuel mass = sum $ unfoldr fuelOfFuel mass
+
+fuelOfFuel :: Int -> Maybe (Int, Int)
+fuelOfFuel mass
+  | mass <= 0 = Nothing
+  | otherwise = let next = fuel mass in
+    if next > 0 then Just (next, next)
+                else Nothing
